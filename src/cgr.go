818b13671d843e4e02b724f218c22679f75e117f
Matthew Wells <mattwells9@shaw.ca>
2024-11-04 18:42:28 -0600

    added cli parser for creating output files

--- src/cgr.go
+++ src/cgr.go
@@ -8,24 +8,19 @@ import (
 	"log/slog"
 	"github.com/golang/protobuf/proto"
 	"path"
-
+	"github.com/integrii/flaggy"
 )
 
 
 func MakeDir(name string) string {
 
-	cwd, err := os.Getwd()
-	if err != nil {
-		slog.Error("Could not get current directory", slog.Any("err", err))
-	}
-
-	output_path := path.Join(cwd, name);
+	output_path := path.Join(name, "cgr");
 	stat, err := os.Stat(output_path)
 	if err == nil && stat.IsDir() {
 		return output_path
 	}
 	slog.Info("Creating directory", slog.String("Path", output_path))
-	err = os.Mkdir(output_path, os.ModePerm)
+	err = os.MkdirAll(output_path, os.ModePerm)
 	if err != nil {
 		slog.Error("Could not make directory", slog.Any("err", err))
 	}
@@ -56,30 +51,75 @@ func WriteProtoBuff(output_obj *CGR, output_dir string){
 
 }
 
+var INPUT_FASTAS string = ""
+var OUTPUT_DIRECTORY string = ""
+var CGR_SIZE uint64 = 1024
+var create_cgr *flaggy.Subcommand
+
+const version string = "0.0.1"
+
+func cli(){
+	flaggy.SetName("Chaos Game Representation with go")
+	flaggy.SetDescription("A tool kit for creating and manipulating chaos game representations of genomes")
+	flaggy.ShowHelpOnUnexpectedEnable()
+
+	create_cgr = flaggy.NewSubcommand("create")
+	create_cgr.Description = "Compute a CGR for all sequences in a multi-fasta"
+	create_cgr.String(&INPUT_FASTAS, "i", "input", "A multifasta file to use for CGR computation")
+	create_cgr.String(&OUTPUT_DIRECTORY, "o", "output-directory", "The output directory for computed CGR files.")
+	cgr_size_help := fmt.Sprintf("The cgr map size to create: %d", CGR_SIZE)
+	create_cgr.UInt64(&CGR_SIZE, "c", "cgr-size", cgr_size_help)
+
+
+	flaggy.AttachSubcommand(create_cgr, 1)
+	flaggy.Parse()
+
+}
+
 func main(){
-	fastas := ReadFasta(os.Args[1])
-	var cgr_size uint64 = 1024
-	rand.Seed(42)
-	x := rand.Uint64() % cgr_size
-	y := rand.Uint64() % cgr_size
-	slog.Debug("Points", slog.Uint64("x", uint64(x)), slog.Uint64("y", uint64(y)))
-	output_path := MakeDir("cgr")
-	for _, f := range fastas {
-		i := *f
-		cgr_map := CreateCGRMap(cgr_size)
-		cgr_map.AddPoint(x, y)
-		for _, nuc := range i.Sequence {
-			slog.Debug("Points",slog.Any("Nuc", nuc), slog.Uint64("x", uint64(x)), slog.Uint64("y", uint64(y)))
-			x, y = cgr_map.NextPoint(x, y, nuc)
-			cgr_map.AddPoint(x, y)
-			slog.Debug("Points", slog.Uint64("x", uint64(x)), slog.Uint64("y", uint64(y)))
-		}
-		output_obj := &CGR{
-			Name: i.Header,
-			Cgr: cgr_map.Data,
-			Size: cgr_map.Size,
+	cli()
+	if len(os.Args) <= 1 {
+		flaggy.ShowHelpAndExit("No inputs passed")
+	}
+
+	if create_cgr.Used {
+		if INPUT_FASTAS == "" {
+			flaggy.ShowHelpAndExit("input fastas is a mandatory argument")
 		}
 
-		WriteProtoBuff(output_obj, output_path)
+		if OUTPUT_DIRECTORY == "" {
+			var err error
+			OUTPUT_DIRECTORY, err = os.Getwd()
+			if err != nil {
+				slog.Error("Could not get current directory", slog.Any("err", err))
+			}
+		}
+
+		fastas := ReadFasta(INPUT_FASTAS)
+		var cgr_size uint64 = CGR_SIZE
+		rand.Seed(42)
+		x := rand.Uint64() % cgr_size
+		y := rand.Uint64() % cgr_size
+		slog.Debug("Points", slog.Uint64("x", uint64(x)), slog.Uint64("y", uint64(y)))
+		output_path := MakeDir(OUTPUT_DIRECTORY)
+		for _, f := range fastas {
+			i := *f
+			cgr_map := CreateCGRMap(cgr_size)
+			cgr_map.AddPoint(x, y)
+			for _, nuc := range i.Sequence {
+				slog.Debug("Points",slog.Any("Nuc", nuc), slog.Uint64("x", uint64(x)), slog.Uint64("y", uint64(y)))
+				x, y = cgr_map.NextPoint(x, y, nuc)
+				cgr_map.AddPoint(x, y)
+				slog.Debug("Points", slog.Uint64("x", uint64(x)), slog.Uint64("y", uint64(y)))
+			}
+
+			output_obj := &CGR{
+				Name: i.Header,
+				Cgr: cgr_map.Data,
+				Size: cgr_map.Size,
+			}
+
+			WriteProtoBuff(output_obj, output_path)
+		}
 	}
 }
--- src/go.mod
+++ src/go.mod
@@ -4,5 +4,6 @@ go 1.22.6
 
 require (
 	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/integrii/flaggy v1.5.2 // indirect
 	google.golang.org/protobuf v1.35.1 // indirect
 )
